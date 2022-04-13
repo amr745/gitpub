7ee24ffdcb86c321cc596efebeed1b1de50b33a6
Ashley Richard <ashleymrichard@gmail.com>
2022-04-13 02:30:41 -0700

    individual drinks rendered in show view

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require ('express');
 const  app = express ();
 const PORT = process.env.PORT || 3001;
 const drinks = require('./models/drinks.js');
-// let titleCaseDrinks = drinks.indexOf(charAt(0).toUpperCase() + drinks.slice(1));
-// const uppercased = drinks.map(drink => drink.toString().toUpperCase());
-
 
 app.get("/", (req,res) => {
     res.send('Welcome to the Gitpub App!')
 });
-
 
 app.get("/drinks", (req,res) => {
     res.render('drinks_index.ejs', {
@@ -18,20 +14,10 @@ app.get("/drinks", (req,res) => {
 });
 
 app.get("/drinks/:id", (req,res) => {
-    res.send(req.params.id)
+    res.render('drinks_show.ejs', {
+       allDrinks: drinks[req.params.id]
+    });
 });
-
-
-    // let allDrinks = drinks.toUpperCase
-    // allDrinks.toUpperCase()
-    // const allDrinks = drinks.charAt(0).toUpperCase() + drinks.slice(1);
-    // let titleCaseDrinks = drinks.map(val => val[0].toUpperCase() + val.slice(1));
-    // const allDrinks = drinks.toUpperCase
-
-
-    // const allDrinks = drinks.charAt(0).toUpperCase() + drinks.slice(1);
-    // drinks[i].charAt(0).toUpperCase()
-// });
 
 app.listen(3000, () => {
     console.log(`Listening on port ${PORT}`)
